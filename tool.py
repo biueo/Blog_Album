a5e7736c73a952f22492f96ba6c99b3e748b9b6e
Kevin596 <decaideng@gmail.com>
2018-02-16 14:36:09 +0800

    add photos

--- tool.py
+++ tool.py
@@ -101,7 +101,8 @@ def handle_photo():
     list_info = []
     for i in range(len(file_list)):
         filename = file_list[i]
-        date_str, info = filename.split("_")
+        date_str, *info = filename.split("_",num=1)
+        info='_'.join(info)
         info, _ = info.split(".")
         date = datetime.strptime(date_str, "%Y-%m-%d")
         year_month = date_str[0:7]            
